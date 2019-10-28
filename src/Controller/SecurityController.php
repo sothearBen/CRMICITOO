5788ee55a442d68e00e104c0d0153f8417dc42c6
nitram1123 <martin3129@gmail.com>
2019-10-28 19:03:10 +0100

    Add registration confirmation email

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,20 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ForgetPasswordFormType;
+use App\Form\ResetPasswordFormType;
+use App\Mailer\Mailer;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Provider\UserAuthenticationProvider;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Exception\LogicException;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use App\Security\LoginFormAuthenticator;
 
 class SecurityController extends AbstractController
 {
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+    
     /**
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
+        //    $this->redirectToRoute('target_path');
         // }
 
         // get the login error if there is one
@@ -32,5 +56,31 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    /**
+     * @Route("/registration_confirm", name="app_registration_confirm")
+     */
+    public function registrationConfirm(Request $request, UserRepository $userRepository, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+    {
+        $token = $request->query->get('token');
+        $user = $userRepository->findOneByConfirmationToken($token);
+        if (null === $user) {
+            throw $this->createNotFoundException(sprintf('The user with confirmation token "%s" does not exist', $token));
+        }
+        
+        $user->setConfirmationToken(null);
+        $user->setEnabled(true);
+        $this->getDoctrine()->getManager()->flush();
+        
+        $msg = $this->translator->trans('registration.flash.confirmed', [ '%user%' => $user, ], 'security');
+        $this->addFlash('success', $msg);
+        
+        return $guardHandler->authenticateUserAndHandleSuccess(
+            $user,
+            $request,
+            $authenticator,
+            'main' // firewall name in security.yaml
+        );
     }
 }
--- src/Mailer/Mailer.php
+++ src/Mailer/Mailer.php
@@ -62,41 +62,12 @@ class Mailer
         $subject = $this->translator->trans('registration.email.subject', [ '%user%' => $user ], 'security');
         $template = 'front/email/register.html.twig';
         $from = [
-            $this->parameters->get('configuration.from_email') => $this->parameters->get('configuration.name'),
+            $this->parameters->get('configuration.')['from_email'] => $this->parameters->get('configuration')['name'],
         ];
         $to = $user->getEmail();
         $body = $this->templating->render($template, [
             'user' => $user,
-            'website_name' => $this->parameters->get('configuration.name'),
-            'confirmation_url' => $url,
-        ]);
-        $message = (new \Swift_Message())
-            ->setSubject($subject)
-            ->setFrom($from)
-            ->setTo($to)
-            ->setContentType("text/html")
-            ->setBody($body);
-        $this->mailer->send($message);
-    }
-
-    public function sendForgetPassword(User $user)
-    {
-        $url = $this->router->generate(
-            'app_reset_password',
-            [
-                'token' => $user->getConfirmationToken(),
-            ],
-            UrlGeneratorInterface::ABSOLUTE_URL
-        );
-        $subject = $this->translator->trans('forget_password.email.subject', [], 'security');
-        $template = 'security/email/forget_password.html.twig';
-        $from = [
-            $this->parameters->get('configuration.from_email') => $this->parameters->get('configuration.name'),
-        ];
-        $to = $user->getEmail();
-        $body = $this->templating->render($template, [
-            'user' => $user,
-            'website_name' => $this->parameters->get('configuration.name'),
+            'website_name' => $this->parameters->get('configuration')['name'],
             'confirmation_url' => $url,
         ]);
         $message = (new \Swift_Message())
