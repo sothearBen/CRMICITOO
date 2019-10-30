5b022edae57433f991671856ff37123dce447b6d
nitram1123 <martin3129@gmail.com>
2019-10-30 18:04:54 +0100

    Add Page Controller Front and Back with index

---
+++ src/Controller/Front/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/", name="front_home")
+     */
+    public function index()
+    {
+        return $this->render('front/page/index.html.twig', [
+            
+        ]);
+    }
+}
