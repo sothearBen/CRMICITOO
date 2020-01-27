7b51bf1bea1c5bd501e9a4209e79cfc0b286eca6
Nitram1123 <martin3129@gmail.com>
2020-01-27 18:47:57 -0500

    Add sendInvitation in service Mailer with template and translation

--- src/Mailer/Mailer.php
+++ src/Mailer/Mailer.php
@@ -50,6 +50,39 @@ class Mailer
         $this->parameters = $parameters;
     }
     
+    public function sendInvitation(User $user, string $password)
+    {
+        $url = $this->router->generate(
+            'app_registration_confirm',
+            [
+                'token' => $user->getConfirmationToken(),
+            ],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+        $subject = $this->translator->trans('invitation.email.subject', [
+            '%user%' => $user,
+            '%website_name%' => $this->parameters->get('configuration')['name'],
+        ], 'back_messages');
+        $template = 'back/email/invite.html.twig';
+        $from = [
+            $this->parameters->get('configuration')['from_email'] => $this->parameters->get('configuration')['name'],
+        ];
+        $to = $user->getEmail();
+        $body = $this->templating->render($template, [
+            'user' => $user,
+            'password' => $password,
+            'website_name' => $this->parameters->get('configuration')['name'],
+            'confirmation_url' => $url,
+        ]);
+        $message = (new \Swift_Message())
+            ->setSubject($subject)
+            ->setFrom($from)
+            ->setTo($to)
+            ->setContentType("text/html")
+            ->setBody($body);
+        $this->mailer->send($message);
+    }
+
     public function sendRegistration(User $user)
     {
         $url = $this->router->generate(
