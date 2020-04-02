3bce4d770615a04dd8243347befcb5a13c97b4bb
Nitram1123 <martin3129@gmail.com>
2020-04-02 03:31:41 +0200

    ...

--- src/Controller/ElFinder/ConnectorController.php
+++ src/Controller/ElFinder/ConnectorController.php
@@ -4,6 +4,7 @@ namespace App\Controller\ElFinder;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -16,10 +17,11 @@ class ConnectorController extends AbstractController
     /**
      * @Route("/connector", name="elfinder_connector")
      */
-    public function index(KernelInterface $kernel, LoggerInterface $logger)
+    public function index(Request $request, KernelInterface $kernel, LoggerInterface $logger)
     {
         $logger->debug(print_r(['start'], true));
         $projectDir = $kernel->getProjectDir();
+        $schemeAndHttpHost = $request->getSchemeAndHttpHost();
         // Documentation for connector options:
         // https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options
         $opts = array(
@@ -29,7 +31,7 @@ class ConnectorController extends AbstractController
                 array(
                     'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
                     'path'          => './files/',
-                    'URL'           => dirname($_SERVER['PHP_SELF']) . '/files/', // URL to files (REQUIRED)
+                    'URL'           => $schemeAndHttpHost . '/files/', // URL to files (REQUIRED)
                     'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
                     'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
                     'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
@@ -42,7 +44,7 @@ class ConnectorController extends AbstractController
                     'id'            => '1',
                     'driver'        => 'Trash',
                     'path'          => './files/.trash/',
-                    'tmbURL'        => dirname($_SERVER['PHP_SELF']) . '/./files/.trash/.tmb/',
+                    'tmbURL'        => $schemeAndHttpHost . '/./files/.trash/.tmb/',
                     'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
                     'uploadDeny'    => array('all'),                // Recomend the same settings as the original volume that uses the trash
                     'uploadAllow'   => array('image/x-ms-bmp', 'image/gif', 'image/jpeg', 'image/png', 'image/x-icon', 'text/plain'), // Same as above
